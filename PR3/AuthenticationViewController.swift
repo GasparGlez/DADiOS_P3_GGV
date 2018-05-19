967f6e21b8b6eb57bf9475c5b6c2061b7699a2f3
GasparGlez <gaspargonzalezvara@gmail.com>
2018-05-19 10:59:12 +0200

    Ej 3

--- PR3/AuthenticationViewController.swift
+++ PR3/AuthenticationViewController.swift
@@ -13,6 +13,18 @@ class AuthenticationViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var thirdField: UITextField!
     @IBOutlet weak var fourthField: UITextField!
     
+    // BEGIN UOC3 - Outlets
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var fourthLabel: UILabel!
+    @IBOutlet weak var upperLabelConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backButtonConstraint: NSLayoutConstraint!
+    @IBOutlet weak var nextButtonConstraint: NSLayoutConstraint!
+    // END UOC3 - Outlets
+    
+    
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         // We first check that the user is only entering numeric characters
         let numericSet = CharacterSet.decimalDigits
@@ -88,6 +100,16 @@ class AuthenticationViewController: UIViewController, UITextFieldDelegate {
         
         if validCode {
             // BEGIN-UOC-3
+            // One second animation
+            UIView.animate(withDuration: 1,
+                           delay: 0,
+                           options: [],
+                           animations: {
+                            self.updateAlphaViews()
+                            self.updateLabelConstraints()
+                            self.view.layoutIfNeeded()
+                            })
+            // Perform segue after animation
             performSegue (withIdentifier: "SegueToMainNavigation", sender: self)
             // END-UOC-3
         } else {
@@ -96,4 +118,26 @@ class AuthenticationViewController: UIViewController, UITextFieldDelegate {
             Utils.show (Message: errorMessage, WithTitle: errorTitle, InViewController: self)
         }
     }
+    
+    // BEGIN - Animation functions UOC-3
+    // Update constraints to animate "movement" of buttons & upper label
+    func updateLabelConstraints() {
+        let screenWidth = view.frame.width
+        let screenHeight = view.frame.height
+        backButtonConstraint.constant = +screenWidth
+        nextButtonConstraint.constant = -screenWidth
+        upperLabelConstraint.constant = -screenHeight
+    }
+    // Update alpha for labels & fields
+    func updateAlphaViews() {
+        firstField.alpha = 0
+        secondField.alpha = 0
+        thirdField.alpha = 0
+        fourthField.alpha = 0
+        firstLabel.alpha = 0
+        secondLabel.alpha = 0
+        thirdLabel.alpha = 0
+        fourthLabel.alpha = 0
+    }
+    // END - Animation functions UOC-3
 }
